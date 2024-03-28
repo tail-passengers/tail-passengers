bcc4ddbac18e8ccc0a3f57831a8c3f7f43833aba
Gape <45617104+jaekkang@users.noreply.github.com>
2024-03-28 18:47:14 +0900

    [Feat] check modal

--- frontend/src/component/navBar.js
+++ frontend/src/component/navBar.js
@@ -21,19 +21,34 @@ export default function renderNavBar() {
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                   <li class="nav-item tp-nav-item">
-                      <a class="nav-link active" href="/selectmode">PONG GAME</a>
+                      <a class="nav-link active" href="/selectmode">${locale.navBar.pongGame}</a>
                   </li>
                   <li class="nav-item tp-nav-item">
-                      <a class="nav-link active" href="/rank">RANK</a>
+                      <a class="nav-link active" href="/rank">${locale.navBar.rank}</a>
                   </li>
                   <li class="nav-item tp-nav-item">
-                      <a class="nav-link active" href="/records">RECORDS</a>
+                      <a class="nav-link active" href="/records">${locale.navBar.records}</a>
                   </li>
               </ul>
               <div class="btn-group" role="group" aria-label="Language">
                   <button id="languageEN" type="button" class="btn btn-secondary ${language === 'en' ? 'active' : ''}">EN</button>
                   <button id="languageKO" type="button" class="btn btn-secondary ${language === 'ko' ? 'active' : ''}">KO</button>
                   <button id="languageJA" type="button" class="btn btn-secondary ${language === 'ja' ? 'active' : ''}">JP</button>
+              </div>
+              <div class="tp-simple-status default-container">
+                <div class="progress tp-winning-percentage" role="progressbar" aria-label="Success example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                  <div class="progress-bar tp-progress-bar" style="width: 25%"></div>
+                </div>
+                <div class="dropdown tp-bg-secondary tp-navbar-dropdown">
+                  <button class="btn tp-bg-secondary dropdown-toggle tp-dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                    PROFILE
+                  </button>
+                  <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
+                    <li><a class="dropdown-item" href="/profile">Your Profile</a></li>
+                    <li><a class="dropdown-item" href="">Settings</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a id="logoutBtn" class="dropdown-item" href="#">Logout</a></li>
+                  </ul>
               </div>
           </div>
       </div>
