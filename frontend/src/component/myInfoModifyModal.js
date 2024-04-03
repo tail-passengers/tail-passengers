3a8914003f26e09fe6508503e85354fc5a331fbe
YunjooCho <73283078+YunjooCho@users.noreply.github.com>
2024-04-03 21:48:45 +0900

    [FEAT] Implementation of change image at Profile Page

--- frontend/src/component/myInfoModifyModal.js
+++ frontend/src/component/myInfoModifyModal.js
@@ -2,22 +2,34 @@ import { $ } from "../utils/querySelector.js";
 import { addProfileModalEventListener } from "../utils/profileEventListener.js";
 
 export default function renderModifyFormModal(myInfo, parentElement) {
-    let contentHTML = `
+	let contentHTML = `
 		<div class="modal tp-sl-card-row tp-modal-div" tabindex="-1">
 			<div class="modal-dialog default-container">
-				<div class="modal-content tp-modal-accept tp-bgc-primary tp-myinfo-modal-content">
+				<div class="modal-content tp-modal-modify tp-bgc-primary tp-myinfo-modal-content default-container">
 					<div class="modal-header tp-friend-modal-header">
 						<h5 class="modal-title tp-color-secondary">Modify My Infomation</h5>
 					</div>
 					<div class="modal-body tp-friend-modal-body default-container">
-						<form class="tp-pf-form-myinfo-update default-container tp-pf-forms tp-sl-card-row">
-								<div class="tp-pf-photo-box">
-									<div class="tp-pf-wd-blank"></div>
-									<img class="tp-pf-photo-thumnail tp-pf-photo-modal-thumnail" src="../../public/assets/img/sharkcookie.png" />
-									<button type="button" class="tp-pf-changeImage-button dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside">
-										<img class="tp-pf-changeImage-thumnail" src="../../public/assets/img/changeProfile.png" />
-									</button>
+						<form class="tp-pf-form-myinfo-image accordion" method="POST" id="accordionImageInput" enctype="multipart/form-data">
+								<div class="accordion-item default-container tp-pf-image-accorion-item">
+									<div class="tp-pf-photo-box accordion-header">
+										<div class="tp-pf-wd-blank"></div>
+										<img class="tp-pf-photo-thumnail tp-pf-photo-modal-thumnail" src="../../public/assets/img/sharkcookie.png" />
+										<button type="button" class="btn tp-pf-changeImage-button" data-bs-toggle="collapse" data-bs-target="#collapseImageForm" aria-expanded="false" aria-controls="collapseImageForm" id="accordionImageInput">
+											<img class="tp-pf-changeImage-thumnail" src="../../public/assets/img/changeProfile.png" />
+										</button>
+									</div>
+									<div id="collapseImageForm" class="accordion-collapse collapse default-container" data-bs-parent="#accordionImageInput">
+										<div class="accordion-body tp-pf-accordion-body">
+											<div class="input-group tp-pf-input-file-grp">
+												<input type="file" name="profileImage" id="imageInput" accept="image/*" class="form-control tp-pf-input tp-pf-input-file" aria-label="Upload">
+												<button type="button" class="btn btn-outline-secondary tp-sl-btn-primary tp-pf-file-btn">Button</button>
+											</div>
+										</div>
+									</div>
 								</div>
+						</form>
+						<form class="tp-pf-form-myinfo-update default-container tp-pf-forms tp-sl-card-row">
 								<div class="tp-pf-ht-blank"></div>
 								<div class="input-group tp-pf-input-group default-container">
 										<div class="tp-pf-elembox tp-pf-nickname">
@@ -32,18 +44,18 @@ export default function renderModifyFormModal(myInfo, parentElement) {
 											<label class="form-label tp-pf-label tp-pf-label-intraId default-container">INTRA ID</label>
 											<input class="form-control tp-pf-input tp-pf-input-intraId tp-pf-modal-intraId" placeholder="test" value="" disabled>
 										</div>
-										<div class="tp-pf-elembox tp-pf-coalitions">
-											<label class="form-label tp-pf-label tp-pf-label-coalitions default-container">COALITIONS</label>
-											<select name="coalitions" class="form-select tp-bg-secondary tp-pf-modal-coalition" aria-label="Default select">
-												<option value="1" selected">Gryffindor</option>
-												<option value="2">Hufflepuff</option>
-												<option value="3">Ravenclaw</option>
-												<option value="4">Slytherin</option>
+										<div class="tp-pf-elembox tp-pf-house">
+											<label class="form-label tp-pf-label tp-pf-label-house default-container">HOUSE</label>
+											<select name="house" class="form-select tp-pf-input tp-bg-secondary tp-pf-modal-house" aria-label="Default select">
+												<option class="tp-pf-house-item tp-pf-house-gr" value="GR">Gryffindor</option>
+												<option class="tp-pf-house-item tp-pf-house-hu" value="HU">Hufflepuff</option>
+												<option class="tp-pf-house-item tp-pf-house-ra" value="RA">Ravenclaw</option>
+												<option class="tp-pf-house-item tp-pf-house-sl" value="SL">Slytherin</option>
 											</select>
 										</div>
 								</div>
 							<div class="tp-pf-ht-blank"></div>
-							<div class="tp-pf-btn-group d-grid gap-2 d-md-flex tp-fl-btn-group">
+							<div class="tp-pf-btn-group d-grid gap-2 d-md-flex tp-fl-btn-group-modal">
 								<div class="tp-sl-btn-parent default-container">
 									<button type="submit" class="btn tp-sl-btn-primary tp-pf-btn tp-fl-submit-modal-btn card default-container h-100 tp-fl-btn" value="SUBMIT" 
 										data-bs-toggle="tooltip" title="Submit"> 
@@ -64,63 +76,42 @@ export default function renderModifyFormModal(myInfo, parentElement) {
 						</form>
 					</div>
 					<div class="modal-footer tp-friend-modal-footer">
-							<div class="dropdown">
-								<button type="button" class="btn tp-pf-changeImage-butto dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside">
-									<img class="tp-pf-changeImage-thumnail" src="../../public/assets/img/changeProfile.png" />
-								</button>
-								<form class="dropdown-menu p-4">
-									<div class="mb-3">
-										<label for="exampleDropdownFormEmail2" class="form-label">Email address</label>
-										<input type="email" class="form-control" id="exampleDropdownFormEmail2" placeholder="email@example.com">
-									</div>
-									<div class="mb-3">
-										<label for="exampleDropdownFormPassword2" class="form-label">Password</label>
-										<input type="password" class="form-control" id="exampleDropdownFormPassword2" placeholder="Password">
-									</div>
-									<div class="mb-3">
-										<div class="form-check">
-											<input type="checkbox" class="form-check-input" id="dropdownCheck2">
-											<label class="form-check-label" for="dropdownCheck2">
-												Remember me
-											</label>
-										</div>
-									</div>
-									<button type="submit" class="btn btn-primary">Sign in</button>
-								</form>
-							</div>
 					</div>
 				</div>
 			</div>
 		</div>
 	`;
 
-    const renderUser = function (content) {
-        const nickname = $(".tp-pf-modal-nickname");
-        const intraId = $(".tp-pf-modal-intraId");
-        const profileImg = $(".tp-pf-photo-modal-thumnail");
-        const record = $(".tp-pf-modal-record");
-        const winCount = $(".tp-pf-modal-wincount");
-        const loseCount = $(".tp-pf-modal-wincount");
-        const coalitions = $(".tp-pf-modal-coalition");
-        nickname.value = content.nickname;
-        intraId.value = content.intra_id;
-        profileImg.src = "http://127.0.0.1:443" + content.profile_image;
-        record.value =
-            content.win_count + " WIN, " + content.lose_count + " LOSE";
-        winCount.value = content.win_count;
-        loseCount.value = content.lose_count;
-        coalitions.value = content.coalitions;
-    };
+	const handlerHouseValue = function(houseName) {
+    const house = $(".tp-pf-modal-house");
+    const options = house.querySelectorAll(".tp-pf-house-item");
+    options.forEach(option => {
+        if (option.value === houseName) {
+            option.selected = true;
+        }
+    });
+	}
 
-    let modifyModal = document
-        .createRange()
-        .createContextualFragment(contentHTML);
-    const prevModal = parentElement.querySelector(".tp-modal-div");
-    if (prevModal) {
-        parentElement.removeChild(prevModal);
-    }
-    parentElement.appendChild(modifyModal);
-    parentElement.style.display = "";
-    addProfileModalEventListener(parentElement);
-    renderUser(myInfo);
+	const renderUser = function (content) {
+		const nickname = $(".tp-pf-modal-nickname");
+		const intraId = $(".tp-pf-modal-intraId");
+		const profileImg = $(".tp-pf-photo-modal-thumnail");
+		const record = $(".tp-pf-modal-record");
+		nickname.value = content.nickname;
+		intraId.value = content.intra_id;
+		profileImg.src = "https://${process.env.BASE_IP}" + content.profile_image;
+		record.value = content.win_count + " WIN, " + content.lose_count + " LOSE";
+		handlerHouseValue(content.house);
+	};
+
+	let modifyModal = document.createRange().createContextualFragment(contentHTML);
+	const prevModal = parentElement.querySelector(".tp-modal-div");
+	if (prevModal)
+	{
+		parentElement.removeChild(prevModal);
+	}
+	parentElement.appendChild(modifyModal);
+	parentElement.style.display = "";
+	renderUser(myInfo);
+	addProfileModalEventListener(parentElement, myInfo);
 }
