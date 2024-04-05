eee79b79dcd2cfc91ae671fc44f1159253072071
seulee2 <croft@kakao.com>
2024-04-05 17:42:49 +0900

    [FEAT] Tornament base page added

--- frontend/src/utils/locales/en.js
+++ frontend/src/utils/locales/en.js
@@ -1,71 +1,80 @@
 const enLocale = {
-  navBar: {
-    brand: "TAIL PASSENGER TEST",
-    pongGame: "PONG GAME",
-    rank: "RANK",
-    records: "RECORDS",
-    profile: "PROFILE",
-    settings: "Settings",
-    logout: "Logout",
-    language: "Language",
-    english: "English",
-    korean: "한국어",
-    japanese: "日本語"
-  },
-  selectMode: {
-    mainText: "Select game mode",
-    singleMode: "Single mode",
-    singleModeDesc: "Single mode against computer",
-    multiMode: "Multi mode",
-    multiModeDesc: "Multi mode against other users"
-  },
-  rank: {
-    mainText: "Top Player",
-    subText: "Thank you for playing",
-    thRank: "Rank",
-    thUserProfile: "Profile",
-    thUserIntraId: "Intra ID",
-    thUserName: "User Name",
-    thWins: "Wins",
-    thLoses: "Loses",
-    thRankPoint: "Rank Point",
-    thFriendRequestBtn: "Friend Request",
-    tootipRequest: "Request"
-  },
-  records: {
-    mainText: "Records",
-  },
-  profileTab: {
-    myInfoTab: "MY",
-    friendListTab: "FRIEND",
-  },
-  friendList: {
-    nofriends: "You have no friends",
-    friendsNumber: "NO",
-    friendsProfile: "PROFILE",
-    friendsNickname: "NICKNAME",
-    friendsConnection: "CONNECTION",
-    friendsRequest: "FRIEND REQUEST",
-    tooltipAccept: "Accept",
-    tooltipRefuse: "Refuse"
-  },
-  myInfo: {
-    myNickname: "NICKNAME",
-    myRecord: "RECORD",
-    myIntraId: "INTRA ID",
-    myHouse: "HOUSE",
-    modifyButton: "MODIFY",
-    win: "WIN",
-    lose: "LOSE",
-    modifyMyInfoTitle: "Modify My Infomation",
-    modifyMyInfoImageButton: "UPLOAD"
-  },
-  house: {
-    griffindor: "Gryffindor",
-    hufflepuff: "Hufflepuff",
-    ravenclaw: "Ravenclaw",
-    slytherin: "Slytherin"
-  }
+	navBar: {
+		brand: "TAIL PASSENGER TEST",
+		pongGame: "PONG GAME",
+		rank: "RANK",
+		records: "RECORDS",
+		profile: "PROFILE",
+		settings: "Settings",
+		logout: "Logout",
+		language: "Language",
+		english: "English",
+		korean: "한국어",
+		japanese: "日本語"
+	},
+	selectMode: {
+		mainText: "Select game mode",
+		singleMode: "Single mode",
+		singleModeDesc: "Single mode against computer",
+		multiMode: "Multi mode",
+		multiModeDesc: "Multi mode against other users",
+		tornamentMode: "Tornament mode",
+		tornamentModeDesc: "Tornament mode with 4 users"
+	},
+	tornament: {
+		mainText: "Tournament List",
+		mainTextDesc: "You can create or join a tournament.",
+		list: "Tournament Name",
+		createBtn: "Create New Tournament",
+		refresh: "Refresh"
+	},
+	rank: {
+		mainText: "Top Player",
+		subText: "Thank you for playing",
+		thRank: "Rank",
+		thUserProfile: "Profile",
+		thUserIntraId: "Intra ID",
+		thUserName: "User Name",
+		thWins: "Wins",
+		thLoses: "Loses",
+		thRankPoint: "Rank Point",
+		thFriendRequestBtn: "Friend Request",
+		tootipRequest: "Request"
+	},
+	records: {
+		mainText: "Records",
+	},
+	profileTab: {
+		myInfoTab: "MY",
+		friendListTab: "FRIEND",
+	},
+	friendList: {
+		nofriends: "You have no friends",
+		friendsNumber: "NO",
+		friendsProfile: "PROFILE",
+		friendsNickname: "NICKNAME",
+		friendsConnection: "CONNECTION",
+		friendsRequest: "FRIEND REQUEST",
+		tooltipAccept: "Accept",
+		tooltipRefuse: "Refuse"
+	},
+	myInfo: {
+		myNickname: "NICKNAME",
+		myRecord: "RECORD",
+		myIntraId: "INTRA ID",
+		myHouse: "HOUSE",
+		modifyButton: "MODIFY",
+		win: "WIN",
+		lose: "LOSE",
+		modifyMyInfoTitle: "Modify My Infomation",
+		modifyMyInfoImageButton: "UPLOAD"
+	},
+	house: {
+		griffindor: "Gryffindor",
+		hufflepuff: "Hufflepuff",
+		ravenclaw: "Ravenclaw",
+		slytherin: "Slytherin"
+	}
 };
 
 export default enLocale;
--- frontend/src/utils/locales/ko.js
+++ frontend/src/utils/locales/ko.js
@@ -1,71 +1,80 @@
 const koLocale = {
-  navBar: {
-    brand: "테일 패신저",
-    pongGame: "퐁 게임",
-    rank: "랭킹",
-    records: "기록",
-    profile: "프로필",
-    settings: "설정",
-    logout: "로그아웃",
-    language: "언어",
-    english: "English",
-    korean: "한국어",
-    japanese: "일본어"
-  },
-  selectMode: {
-    mainText: "게임 모드를 선택하세요",
-    singleMode: "싱글 모드",
-    singleModeDesc: "컴퓨터와 대결하는 싱글 모드",
-    multiMode: "멀티 모드",
-    multiModeDesc: "다른 유저와 대결하는 멀티 모드"
-  },
-  rank: {
-    mainText: "최고 플레이어",
-    subText: "플레이해 주셔서 감사합니다",
-    thRank: "순위",
-    thUserProfile: "프로필 사진",
-    thUserIntraId: "인트라 아이디",
-    thUserName: "사용자 이름",
-    thWins: "승리",
-    thLoses: "패배",
-    thRankPoint: "랭크 포인트",
-    thFriendRequestBtn: "친구 신청",
-    tootipRequest: "신청"
-  },
-  records: {
-    mainText: "기록",
-  },
-  profileTab: {
-    myInfoTab: "내 정보",
-    friendListTab: "친구목록",
-  },
-  friendList: {
-    nofriends: "친구가 없습니다",
-    friendsNumber: "번호",
-    friendsProfile: "프로필 사진",
-    friendsNickname: "닉네임",
-    friendsConnection: "접속 상태",
-    friendsRequest: "친구 신청",
-    tooltipAccept: "수락",
-    tooltipRefuse: "거절"
-  },
-  myInfo: {
-    myNickname: "닉네임",
-    myRecord: "기록",
-    myIntraId: "인트라 아이디",
-    myHouse: "기숙사",
-    modifyButton: "수정",
-    win: "승",
-    lose: "패",
-    modifyMyInfoTitle: "회원 정보 수정",
-    modifyMyInfoImageButton: "업로드"
-  },
-  house: {
-    griffindor: "그리핀도르",
-    hufflepuff: "후플푸프",
-    ravenclaw: "래번클로",
-    slytherin: "슬리데린"
-  }
+	navBar: {
+		brand: "테일 패신저",
+		pongGame: "퐁 게임",
+		rank: "랭킹",
+		records: "기록",
+		profile: "프로필",
+		settings: "설정",
+		logout: "로그아웃",
+		language: "언어",
+		english: "English",
+		korean: "한국어",
+		japanese: "일본어"
+	},
+	selectMode: {
+		mainText: "게임 모드를 선택하세요",
+		singleMode: "싱글 모드",
+		singleModeDesc: "컴퓨터와 대결하는 싱글 모드",
+		multiMode: "멀티 모드",
+		multiModeDesc: "다른 유저와 대결하는 멀티 모드",
+		tornamentMode: "토너먼트 모드",
+		tornamentModeDesc: "4명과 대결하는 토너먼트 모드"
+	},
+	tornament: {
+		mainText: "토너먼트 목록",
+		mainTextDesc: "새 토너먼트를 생성하거나 참가할 수 있습니다.",
+		list: "토너먼트 이름",
+		createBtn: "새 토너먼트 만들기",
+		refresh: "새로고침"
+	},
+	rank: {
+		mainText: "최고 플레이어",
+		subText: "플레이해 주셔서 감사합니다",
+		thRank: "순위",
+		thUserProfile: "프로필 사진",
+		thUserIntraId: "인트라 아이디",
+		thUserName: "사용자 이름",
+		thWins: "승리",
+		thLoses: "패배",
+		thRankPoint: "랭크 포인트",
+		thFriendRequestBtn: "친구 신청",
+		tootipRequest: "신청"
+	},
+	records: {
+		mainText: "기록",
+	},
+	profileTab: {
+		myInfoTab: "내 정보",
+		friendListTab: "친구목록",
+	},
+	friendList: {
+		nofriends: "친구가 없습니다",
+		friendsNumber: "번호",
+		friendsProfile: "프로필 사진",
+		friendsNickname: "닉네임",
+		friendsConnection: "접속 상태",
+		friendsRequest: "친구 신청",
+		tooltipAccept: "수락",
+		tooltipRefuse: "거절"
+	},
+	myInfo: {
+		myNickname: "닉네임",
+		myRecord: "기록",
+		myIntraId: "인트라 아이디",
+		myHouse: "기숙사",
+		modifyButton: "수정",
+		win: "승",
+		lose: "패",
+		modifyMyInfoTitle: "회원 정보 수정",
+		modifyMyInfoImageButton: "업로드"
+	},
+	house: {
+		griffindor: "그리핀도르",
+		hufflepuff: "후플푸프",
+		ravenclaw: "래번클로",
+		slytherin: "슬리데린"
+	}
 };
 
 export default koLocale;
