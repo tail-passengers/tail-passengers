e786413c926cfa95ad2978c3c48c1831dbb2aba2
Gape <45617104+jaekkang@users.noreply.github.com>
2024-04-05 16:20:43 +0900

    [Feat] add default chart

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,3 +1,5 @@
+import Chart from 'chart.js/auto';
+
 function Home($container) {
     this.$container = $container;
     
@@ -20,6 +22,38 @@ function Home($container) {
                 </div>
             </div>
         `;
+
+        const ctx = document.getElementById('bar-chart').getContext('2d');
+        const myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ["Africa", "Asia", "Europe", "Latin America"],
+                datasets: [{
+                    label: 'Population (millions)',
+                    data: [2478, 5267, 734, 784],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
     };
     this.render();
 }
