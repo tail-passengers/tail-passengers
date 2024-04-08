b9b8bec7a3b168d81f078d1438396f8ef01ce6f8
Gape <45617104+jaekkang@users.noreply.github.com>
2024-04-08 16:10:15 +0900

    [Feat] add chart container & changed login modal css (#10)
    * [Feat] add chart + records page logic changed
    
    * [Feat] add default chart
    
    * [Feat] add user records format
    
    * [Feat] edit makefile
    
    * [Feat] changed login modal css
    
    * [Feat] remove font-family setting

--- frontend/src/component/loginModal.js
+++ frontend/src/component/loginModal.js
@@ -1,22 +1,13 @@
 export default function renderLoginModal() {
     return `
 	<div class="modal">
-		<div class="modal__background"></div>
 		<div class="modal__content">
-			<div class="content default-container">
-			<div class="sized-box"></div>
-			<div class="card lg_card lg_bgc">
-				<div class="row g-0">
-					<div class="col-md-4">
-						<img src="../public/assets/img/ticket.png" class="img-fluid" alt="ticket">
-						<img src="../public/assets/img/ticket_small.png" class="img-fluid" alt="ticket_small">
-					</div>
-					<div class="col-md-8">
-						<div class="d-flex justify-content-center">
-							<a href="https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-a386dd1a6ec3e1df7bbd427c9ebe30d4d811b69b473188d562bc342e059962db&redirect_uri=https%3A%2F%2F127.0.0.1%2Fapi%2Fv1%2Flogin%2F42%2Fcallback%2F&response_type=code" id="login-btn">Login</a>
-							<button class="button" id="close-btn">Close</button>
-						</div>
-					</div>
+			<div class="back color-6">
+				<div class="row columns position-relative">
+					<img src="../../public/assets/img/ticket_small.png" class="img-fluid" alt="ticket_small">
+					<ul class="menu align-center expanded text-center SMN_effect-16">
+						<li><a href="https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-a386dd1a6ec3e1df7bbd427c9ebe30d4d811b69b473188d562bc342e059962db&redirect_uri=https%3A%2F%2F127.0.0.1%2Fapi%2Fv1%2Flogin%2F42%2Fcallback%2F&response_type=code">SIGN</a></li>
+					</ul>
 				</div>
 			</div>
 		</div >
--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,27 +1,109 @@
+import Chart from 'chart.js/auto';
+
 function Home($container) {
-	this.$container = $container;
+    this.$container = $container;
+    this.$chartCanvas = null;
+    this.gameLogs = [];
+    this.myChart = null;
 
-	this.setState = () => {
-		this.render();
-	};
+    this.setState = () => {
+        this.render();
+    };
 
-	this.render = () => {
-		this.$container.innerHTML = `
-      <div class="content default-container">
-        <div class="sized-box"></div>
-        <div class = "default-container home-top-container">
-        </div>
-        <div class="sized-box"></div>
-        <div class="sized-box"></div>
-        <div class = "home-bottom-container">
-          <div></div>
-          <div></div>
-        </div>
-      </div>
-	  `;
-	};
+    this.render = () => {
+        this.$container.innerHTML = `
+            <div class="content default-container">
+                <div class="sized-box"></div>
+                <div class="sized-box"></div>
+                <div class="home-top-container">
+                    <canvas id="bar-chart" style="flex:1;"></canvas>
+                    <div id="records-box">
+                        <ul id="records-list"></ul>
+                    </div>
+                </div>
+                <div class="sized-box"></div>
+                <div class="sized-box"></div>
+                <div class="home-bottom-container">
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        `;
 
-	this.render();
+        this.$chartCanvas = document.getElementById('bar-chart');
+
+        this.generateGameLogs();
+
+        this.renderChart();
+        this.renderRecords();
+    };
+
+    this.generateGameLogs = () => {
+        this.gameLogs = [
+            { winner: 'Player1', loser: 'Player2', start_time: new Date().toISOString() },
+            { winner: 'Player2', loser: 'Player3', start_time: new Date().toISOString() },
+            { winner: 'Player3', loser: 'Player4', start_time: new Date().toISOString() },
+            { winner: 'Player4', loser: 'Player5', start_time: new Date().toISOString() },
+            { winner: 'Player5', loser: 'Player6', start_time: new Date().toISOString() },
+        ];
+    };
+
+    this.renderRecords = () => {
+        const recordsList = document.getElementById('records-list');
+        recordsList.innerHTML = ''; 
+
+        const limitedGameLogs = this.gameLogs.slice(0, 5);
+        
+        limitedGameLogs.forEach(log => {
+            const li = document.createElement('li');
+            li.textContent = `${log.winner} vs ${log.loser}`;
+            li.style.color = 'black';
+            recordsList.appendChild(li);
+        });
+    };
+
+    this.renderChart = () => {
+        if (this.myChart) {
+            this.myChart.destroy();
+        }
+    
+        const width = this.$chartCanvas.offsetWidth;
+        const height = this.$chartCanvas.offsetHeight;
+
+        this.$chartCanvas.width = width;
+        this.$chartCanvas.height = height;
+
+        const ctx = this.$chartCanvas.getContext('2d');
+        this.myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ["Africa", "Asia", "Europe", "Latin America"],
+                datasets: [{
+                    label: 'Population (millions)',
+                    data: [2478, 5267, 734, 784],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.5)',
+                        'rgba(54, 162, 235, 0.5)',
+                        'rgba(255, 206, 86, 0.5)',
+                        'rgba(75, 192, 192, 0.5)',
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                    ],
+                    borderWidth: 1
+                }]
+            }
+        });
+    };
+
+    window.addEventListener('resize', () => {
+        this.renderChart();
+    });
+
+    this.render();
 }
 
 export default Home;
