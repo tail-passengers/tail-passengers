c79f20ef791786261d49ed1e1a383b317ab66ade
Gape <45617104+jaekkang@users.noreply.github.com>
2024-04-10 15:50:32 +0900

    [Feat] Login logic changed (#14)

--- frontend/src/component/loginModal.js
+++ frontend/src/component/loginModal.js
@@ -6,7 +6,7 @@ export default function renderLoginModal() {
 				<div class="row columns position-relative">
 					<img src="../../public/assets/img/ticket_small.png" class="img-fluid" alt="ticket_small">
 					<ul class="menu align-center expanded text-center SMN_effect-16">
-						<li><a id="sign-link">SIGN</a></li>
+                    <li><a href="/api/v1/login">SIGN</a></li>
 					</ul>
 				</div>
 			</div>
@@ -14,24 +14,3 @@ export default function renderLoginModal() {
     </div>
 	`;
 }
-
-document.addEventListener('DOMContentLoaded', () => {
-    const signLink = document.getElementById('sign-link');
-    signLink.addEventListener('click', () => {
-        fetch(`https://${process.env.BASE_IP}/api/v1/login/`, {
-            method: 'GET',
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log(data); 
-        })
-        .catch(error => {
-            console.error('There was a problem with the fetch operation:', error);
-        });
-    });
-});
--- frontend/src/utils/routeInfo.js
+++ frontend/src/utils/routeInfo.js
@@ -4,7 +4,6 @@ import NotFound from "../pages/notFound.js";
 import SelectMode from "../pages/selectMode.js";
 import Rank from "../pages/rank.js";
 import Profile from "../pages/profile.js";
-import AboutUs from "../pages/aboutus.js";
 import SocketTest from "../pages/example_websocket.js";
 import RecordsSearch from "../pages/recordsSearch.js";
 import Tornament from "../pages/tornament.js";
@@ -14,7 +13,6 @@ export const routes = [
 	{ path: /^\/game$/, element: Game },
 	{ path: /^\/rank$/, element: Rank },
 	{ path: /^\/records$/, element: RecordsSearch },
-	{ path: /^\/aboutus$/, element: AboutUs },
 	{ path: /^\/selectmode$/, element: SelectMode },
 	{ path: /^\/profile$/, element: Profile },
 	{ path: /^\/general_game\/(.+)$/, element: SocketTest },
