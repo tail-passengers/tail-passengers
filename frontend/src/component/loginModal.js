72ac3c3ebeade72d9b219e95debd6f762d38471e
Gape <45617104+jaekkang@users.noreply.github.com>
2024-04-10 13:48:41 +0900

    [Feat] login modal logic changed

--- frontend/src/component/loginModal.js
+++ frontend/src/component/loginModal.js
@@ -6,7 +6,7 @@ export default function renderLoginModal() {
 				<div class="row columns position-relative">
 					<img src="../../public/assets/img/ticket_small.png" class="img-fluid" alt="ticket_small">
 					<ul class="menu align-center expanded text-center SMN_effect-16">
-						<li><a href="https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-a386dd1a6ec3e1df7bbd427c9ebe30d4d811b69b473188d562bc342e059962db&redirect_uri=https%3A%2F%2F127.0.0.1%2Fapi%2Fv1%2Flogin%2F42%2Fcallback%2F&response_type=code">SIGN</a></li>
+						<li><a id="sign-link">SIGN</a></li>
 					</ul>
 				</div>
 			</div>
@@ -14,3 +14,24 @@ export default function renderLoginModal() {
     </div>
 	`;
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const signLink = document.getElementById('sign-link');
+    signLink.addEventListener('click', () => {
+        fetch(`https://${process.env.BASE_IP}/api/v1/login/`, {
+            method: 'GET',
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data); 
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+    });
+});
