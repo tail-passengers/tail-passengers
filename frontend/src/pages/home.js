c34e8f35b8096e2c7fa70203ca5d74c8eacc1f4a
JaeKwon Kang <45617104+jaekkang@users.noreply.github.com>
2024-04-12 20:38:16 +0900

    [Feat] chart render logic changed

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,16 +1,21 @@
-import Chart from 'chart.js/auto';
+import Chart from "chart.js/auto";
+import { fetchChartData } from "../utils/fetches";
+import { getCurrentLanguage } from "../utils/languageUtils.js";
+import locales from "../utils/locales/locales.js";
 
 function Home($container) {
     this.$container = $container;
     this.$chartCanvas = null;
     this.gameLogs = [];
     this.myChart = null;
+    const language = getCurrentLanguage();
+    const locale = locales[language] || locales.en;
 
     this.setState = () => {
         this.render();
     };
 
-    this.render = () => {
+    this.render = async () => {
         this.$container.innerHTML = `
             <div class="content default-container">
                 <div class="sized-box"></div>
@@ -30,77 +35,118 @@ function Home($container) {
             </div>
         `;
 
-        this.$chartCanvas = document.getElementById('bar-chart');
+        this.$chartCanvas = document.getElementById("bar-chart");
 
-        this.generateGameLogs();
+        const apiResponse = await fetchChartData();
+        const houseRates = apiResponse.house;
+        const userRates = apiResponse.rate;
 
-        this.renderChart();
+        this.renderChart(houseRates, userRates, locale);
         this.renderRecords();
     };
 
-    this.generateGameLogs = () => {
-        this.gameLogs = [
-            { winner: 'Player1', loser: 'Player2', start_time: new Date().toISOString() },
-            { winner: 'Player2', loser: 'Player3', start_time: new Date().toISOString() },
-            { winner: 'Player3', loser: 'Player4', start_time: new Date().toISOString() },
-            { winner: 'Player4', loser: 'Player5', start_time: new Date().toISOString() },
-            { winner: 'Player5', loser: 'Player6', start_time: new Date().toISOString() },
-        ];
-    };
-
     this.renderRecords = () => {
-        const recordsList = document.getElementById('records-list');
-        recordsList.innerHTML = ''; 
+        const recordsList = document.getElementById("records-list");
+        recordsList.innerHTML = "";
 
         const limitedGameLogs = this.gameLogs.slice(0, 5);
-        
-        limitedGameLogs.forEach(log => {
-            const li = document.createElement('li');
+
+        limitedGameLogs.forEach((log) => {
+            const li = document.createElement("li");
             li.textContent = `${log.winner} vs ${log.loser}`;
-            li.style.color = 'black';
+            li.style.color = "black";
             recordsList.appendChild(li);
         });
     };
 
-    this.renderChart = () => {
-        if (this.myChart) {
-            this.myChart.destroy();
-        }
-    
-        const width = this.$chartCanvas.offsetWidth;
-        const height = this.$chartCanvas.offsetHeight;
+    this.renderChart = (houseRates, userRates, locale) => {
+        const labels = Object.keys(houseRates);
+        const datasets = [];
 
-        this.$chartCanvas.width = width;
-        this.$chartCanvas.height = height;
+        const houseDataset = {
+            label: "House",
+            data: [],
+            backgroundColor: "rgba(255, 99, 132, 0.5)",
+            borderColor: "rgba(255, 99, 132, 1)",
+            borderWidth: 1,
+        };
 
-        const ctx = this.$chartCanvas.getContext('2d');
-        this.myChart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: ["Africa", "Asia", "Europe", "Latin America"],
-                datasets: [{
-                    label: 'Population (millions)',
-                    data: [2478, 5267, 734, 784],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.5)',
-                        'rgba(54, 162, 235, 0.5)',
-                        'rgba(255, 206, 86, 0.5)',
-                        'rgba(75, 192, 192, 0.5)',
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                    ],
-                    borderWidth: 1
-                }]
-            }
+        const rateDataset = {
+            label: "My_rate",
+            data: [],
+            backgroundColor: "rgba(54, 162, 235, 0.5)",
+            borderColor: "rgba(54, 162, 235, 1)",
+            borderWidth: 1,
+        };
+
+        labels.forEach((label) => {
+            houseDataset.data.push((houseRates[label] * 100).toFixed(2));
+            rateDataset.data.push((userRates[label] * 100).toFixed(2));
         });
+
+        rateDataset.data.push((userRates.total * 100).toFixed(2));
+        datasets.push(houseDataset);
+        datasets.push(rateDataset);
+
+        if (this.myChart) {
+            this.myChart.data.labels = labels;
+            this.myChart.data.datasets = datasets;
+            this.myChart.options.plugins.title.text = locale.chartTitle;
+            this.myChart.options.scales.y.ticks.stepSize = 20;
+            this.myChart.update();
+        } else {
+            const ctx = this.$chartCanvas.getContext("2d");
+            this.myChart = new Chart(
+                ctx,
+                {
+                    type: "bar",
+                    data: {
+                        labels: labels,
+                        datasets: datasets,
+                    },
+                    options: {
+                        scales: {
+                            y: {
+                                beginAtZero: true,
+                                title: {
+                                    display: true,
+                                    text: "Winning Rate (%)",
+                                },
+                                ticks: {
+                                    stepSize: 20,
+                                },
+                            },
+                        },
+                        plugins: {
+                            title: {
+                                display: true,
+                                text: locale.chartTitle,
+                                font: {
+                                    size: 18,
+                                },
+                            },
+                            tooltip: {
+                                callbacks: {
+                                    label: function (context) {
+                                        const datasetLabel =
+                                            context.dataset.label || "";
+                                        const value = context.parsed.y;
+                                        return `${datasetLabel}: ${value.toFixed(
+                                            2
+                                        )}%`;
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+                500
+            );
+        }
     };
 
-    window.addEventListener('resize', () => {
-        this.renderChart();
+    window.addEventListener("resize", async () => {
+        await this.renderChart();
     });
 
     this.render();
