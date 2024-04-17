ec39fae2f9bc763d11cee05617b0f88c2cbfff16
JaeKwon Kang <45617104+jaekkang@users.noreply.github.com>
2024-04-17 18:37:45 +0900

    [Feat] add records page

--- frontend/src/utils/fetches.js
+++ frontend/src/utils/fetches.js
@@ -2,37 +2,37 @@ import { deleteCSRFToken, getCSRFToken } from "./cookie.js";
 import { navigate } from "./navigate.js";
 
 export const fetchGenenralGameResult = async (gameName) => {
-	try {
-		const response = await fetch(
-			`https://${process.env.BASE_IP}/api/v1/general_game_logs/${gameName}/`,
-			{
-				credentials: "include",
-			}
-		);
+    try {
+        const response = await fetch(
+            `https://${process.env.BASE_IP}/api/v1/general_game_logs/${gameName}/`,
+            {
+                credentials: "include",
+            }
+        );
 
-		const data = await response.json();
-		console.log("fetchGenenralGameResult", data);
-		// return data[0].intra_id;
-	} catch (error) {
-		console.error("Error fetching user data:", error);
-	}
+        const data = await response.json();
+        console.log("fetchGenenralGameResult", data);
+        // return data[0].intra_id;
+    } catch (error) {
+        console.error("Error fetching user data:", error);
+    }
 };
 
 export const fetchTournamentGameResult = async (gameName) => {
-	try {
-		const response = await fetch(
-			`https://${process.env.BASE_IP}/api/v1/tournament_game_logs/tournament/${gameName}/`,
-			{
-				credentials: "include",
-			}
-		);
+    try {
+        const response = await fetch(
+            `https://${process.env.BASE_IP}/api/v1/tournament_game_logs/tournament/${gameName}/`,
+            {
+                credentials: "include",
+            }
+        );
 
-		const data = await response.json();
-		console.log("fetchTournamentGameResult", data);
-		// return data[0].intra_id;
-	} catch (error) {
-		console.error("Error fetching user data:", error);
-	}
+        const data = await response.json();
+        console.log("fetchTournamentGameResult", data);
+        // return data[0].intra_id;
+    } catch (error) {
+        console.error("Error fetching user data:", error);
+    }
 };
 
 /**
@@ -40,19 +40,19 @@ export const fetchTournamentGameResult = async (gameName) => {
  * @returns 나의 intraId
  */
 export const fetchMyIntraId = async () => {
-	try {
-		const response = await fetch(
-			`https://${process.env.BASE_IP}/api/v1/me/`,
-			{
-				credentials: "include",
-			}
-		);
+    try {
+        const response = await fetch(
+            `https://${process.env.BASE_IP}/api/v1/me/`,
+            {
+                credentials: "include",
+            }
+        );
 
-		const data = await response.json();
-		return data[0].intra_id;
-	} catch (error) {
-		console.error("Error fetching user data:", error);
-	}
+        const data = await response.json();
+        return data[0].intra_id;
+    } catch (error) {
+        console.error("Error fetching user data:", error);
+    }
 };
 
 /**
@@ -60,13 +60,13 @@ export const fetchMyIntraId = async () => {
  * @returns 나의 회원 정보
  */
 export const fetchUser = async () => {
-	try {
-		const response = await fetch(
-			`https://${process.env.BASE_IP}/api/v1/me/`,
-			{
-				credentials: "include",
-			}
-		);
+    try {
+        const response = await fetch(
+            `https://${process.env.BASE_IP}/api/v1/me/`,
+            {
+                credentials: "include",
+            }
+        );
 
         if (response.status === 200) {
             const data = await response.json();
@@ -372,19 +372,32 @@ export const fetchChartData = async () => {
 
 export const fetchGameLogs = async () => {
     try {
-        const response = await fetch(
+        const response1 = await fetch(
             `https://${process.env.BASE_IP}/api/v1/general_game_logs/me/`
         );
+        const response2 = await fetch(
+            `https://${process.env.BASE_IP}/api/v1/tournament_game_logs/me/`
+        );
 
-        if (response.ok) {
-            const data = await response.json();
-            return data;
-        } else {
-            console.error("Error fetching chart data: ", response.statusText);
-            return {};
+        let data1 = [];
+        let data2 = [];
+
+        if (response1.ok) {
+            data1 = await response1.json();
         }
+        if (response2.ok) {
+            data2 = await response2.json();
+        }
+
+        const combinedData = [...data1, ...data2];
+
+        combinedData.sort(
+            (a, b) => new Date(a.start_time) - new Date(b.start_time)
+        );
+
+        return combinedData;
     } catch (error) {
-        console.error("Error fetching chart data:", error);
-        return {};
+        console.error("Error fetching game logs:", error);
+        return [];
     }
 };
