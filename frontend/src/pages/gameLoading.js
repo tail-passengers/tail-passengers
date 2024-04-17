9b93d151c811af4a3b79489a52e8adc9f1378b5c
YunjooCho <73283078+YunjooCho@users.noreply.github.com>
2024-04-17 16:16:56 +0900

    [Feat] add loading page

--- frontend/src/pages/gameLoading.js
+++ frontend/src/pages/gameLoading.js
@@ -3,6 +3,7 @@ import locales from "../utils/locales/locales";
 
 function Loading($container) {
     this.$container = $container;
+    let gameIdValue, initSocket = null;
 
     this.setState = () => {
         this.render();
@@ -13,11 +14,44 @@ function Loading($container) {
         const locale = locales[language] || locales.en;
 
         this.$container.innerHTML = `
-      <div class="container tp-color-secondary">
-        <div class="default-container h2">Loading</div>
-		<button id="backBtn" class="btn tp-btn-primary">${locale.loading.back}</button>
-      </div>
-    `;
+          <div class="content container tp-color-secondary">
+            <div class="default-container h2">Loading</div>
+            <button id="backBtn" class="btn tp-btn-primary">${locale.loading.back}</button>
+          </div>
+        `;
+
+        const backBtn = document.getElementById("backBtn");
+        backBtn.addEventListener("click", (event) => {
+          event.preventDefault();
+          history.back();
+        });
+
+        if (initSocket != null) {
+          initSocket.close();
+          initSocket = null;
+        }
+        initSocket = new WebSocket(`wss://${process.env.BASE_IP}/ws/general_game/wait/`);
+        initSocket.addEventListener('message', idSocketConnect);
+      }
+      function closeSocket() {
+        //게임중 뒤로가기면 소켓 닫기, 아닌 경우는 직접 소켓 처리
+        if (initSocket && initSocket.readyState <= 1) {
+            initSocket.close();
+            window.removeEventListener("popstate", closeSocket);
+        }
+        window.addEventListener("popstate", closeSocket);
+
+      const idSocketConnect = (event) => {
+        initSocket.close();
+        let data = JSON.parse(event.data);
+        gameIdValue = data.game_id;
+        // 현재 연결된 소켓을 세션 스토리지에 저장합니다.
+        sessionStorage.setItem("idValue", gameIdValue);
+        sessionStorage.setItem("gameMode", "general_game");
+
+        targetURL = `https://${process.env.BASE_IP}/general_game/${gameIdValue}`;
+        navigate(targetURL);
+    };
     };
 
     this.render();
--- frontend/src/pages/selectMode.js
+++ frontend/src/pages/selectMode.js
@@ -4,9 +4,7 @@ import { getCurrentLanguage } from "../utils/languageUtils.js";
 import locales from "../utils/locales/locales.js";
 
 function SelectMode({ initialState }) {
-    let gameIdValue,
-        initSocket = null,
-        targetURL;
+    let targetURL;
     this.state = initialState;
     this.$element = document.createElement("div");
     this.$element.className = "content default-container tp-sl-card-content";
@@ -17,14 +15,6 @@ function SelectMode({ initialState }) {
         this.state = content;
         this.render();
     };
-
-    function closeSocket() {
-        //게임중 뒤로가기면 소켓 닫기, 아닌 경우는 직접 소켓 처리
-        if (initSocket && initSocket.readyState <= 1) {
-            initSocket.close();
-            window.removeEventListener("popstate", closeSocket);
-        }
-    }
 
     this.render = () => {
         this.$element.innerHTML = `
@@ -69,7 +59,6 @@ function SelectMode({ initialState }) {
         const singleBtn = $(".tp-sl-single-btn");
         const multiBtn = $(".tp-sl-multi-btn");
         const tournamentBtn = $(".tp-sl-tournament-btn");
-        window.addEventListener("popstate", closeSocket);
 
         if (singleBtn) {
             singleBtn.addEventListener("click", function (event) {
@@ -91,30 +80,12 @@ function SelectMode({ initialState }) {
                 event.preventDefault();
                 targetURL = `https://${process.env.BASE_IP}/loading`;
                 navigate(targetURL);
-                // event.preventDefault();
-                // const multiTextElement = document.querySelector(".tp-sl-multi-btn .tp-sl-card-text");
-                // multiTextElement.textContent = `${locale.tournament.waiting}`;
-                // if (initSocket != null) {
-                // 	initSocket.close();
-                // 	initSocket = null;
-                // }
-                // initSocket = new WebSocket(`wss://${process.env.BASE_IP}/ws/general_game/wait/`);
-                // initSocket.addEventListener('message', idSocketConnect);
+                
             });
         }
     };
 
-    const idSocketConnect = (event) => {
-        initSocket.close();
-        let data = JSON.parse(event.data);
-        gameIdValue = data.game_id;
-        // 현재 연결된 소켓을 세션 스토리지에 저장합니다.
-        sessionStorage.setItem("idValue", gameIdValue);
-        sessionStorage.setItem("gameMode", "general_game");
-
-        targetURL = `https://${process.env.BASE_IP}/general_game/${gameIdValue}`;
-        navigate(targetURL);
-    };
+    
 
     this.init = () => {
         let parent = $("#app");
