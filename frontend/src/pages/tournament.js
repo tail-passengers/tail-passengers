fb34ad4aae83fd0a73fa6cdc78803158c558dd97
sngsho <96572410+sngsho@users.noreply.github.com>
2024-04-18 14:55:58 +0900

    토너먼트 이름 특수문자 사용 금지

--- frontend/src/pages/tournament.js
+++ frontend/src/pages/tournament.js
@@ -4,24 +4,24 @@ import { getCurrentLanguage } from "../utils/languageUtils.js";
 import locales from "../utils/locales/locales.js";
 
 function Tournament({ $app, initialState }) {
-    let gameSocket,
-        tournamentName,
-        round,
-        playerNum = 0,
-        data,
-        nickname = "",
-        versusNickname;
+	let gameSocket,
+		tournamentName,
+		round,
+		playerNum = 0,
+		data,
+		nickname = "",
+		versusNickname;
 
-    function closeSocket() {
-        if (gameSocket && gameSocket.readyState <= 1) {
-            gameSocket.close();
-            window.removeEventListener("popstate", closeSocket);
-        }
-    }
-    this.render = () => {
-        const language = getCurrentLanguage();
-        const locale = locales[language] || locales.en;
-        this.$element.innerHTML = `
+	function closeSocket() {
+		if (gameSocket && gameSocket.readyState <= 1) {
+			gameSocket.close();
+			window.removeEventListener("popstate", closeSocket);
+		}
+	}
+	this.render = () => {
+		const language = getCurrentLanguage();
+		const locale = locales[language] || locales.en;
+		this.$element.innerHTML = `
 					<div class="content default-container">
 							<div class="container">
 									<div class="mb-3 mt-3">
@@ -57,206 +57,211 @@ function Tournament({ $app, initialState }) {
 							</div>
 					</div>
 			`;
-        const createTournamentBtn = this.$element.querySelector(
-            "#createTournamentBtn"
-        );
-        createTournamentBtn.addEventListener("click", () => {
-            tournamentName = prompt(`${locale.tournament.noticePrompt}`);
-            const message = {
-                message_type: "create",
-                tournament_name: tournamentName,
-            };
-            gameSocket.send(JSON.stringify(message));
-        });
-        const refreshBtn = this.$element.querySelector("#refreshBtn");
-        refreshBtn.addEventListener("click", () => {
-            this.render();
-        });
+		const createTournamentBtn = this.$element.querySelector(
+			"#createTournamentBtn"
+		);
+		createTournamentBtn.addEventListener("click", () => {
+			tournamentName = prompt(`${locale.tournament.noticePrompt}`);
+			const message = {
+				message_type: "create",
+				tournament_name: tournamentName,
+			};
+			const regex = /^[`~!@#$%^&*()_+-={},./<>?;':"\s\t\n\r]+|\s+$/g;
+			if (regex.test(tournamentName) || tournamentName.length > 20) {
+				alert("Strings must not consist solely of whitespace or be longer than 20 characters.");
+			}
+			else {
+				gameSocket.send(JSON.stringify(message));
+			}
+		});
+		const refreshBtn = this.$element.querySelector("#refreshBtn");
+		refreshBtn.addEventListener("click", () => {
+			this.render();
+		});
 
-        this.getTournamentList();
-    };
+		this.getTournamentList();
+	};
 
-    // 서버로 wait 소켓 연결하고 토너먼트 리스트 불러옴
-    this.getTournamentList = async () => {
-        try {
-            await this.connectWebSocket("wait");
-            gameSocket.addEventListener("message", (event) => {
-                const data = JSON.parse(event.data);
-                if (data.game_list) {
-                    // 토너먼트 리스트를 받은 경우
-                    this.fillTable(data.game_list);
-                } else if (data.message_type === "create") {
-                    if (data.result === "success") {
-                        // 방 생성 성공 시 처리
-                        this.renderWaiting();
-                    } else if (data.result === "fail") {
-                        // 방 생성 실패 시 처리
-                        alert(`${locale.tournament.createFail}`);
-                        this.render();
-                    }
-                }
-            });
-        } catch (error) {
-            console.error("Error socket connect:", error);
-        }
-    };
+	// 서버로 wait 소켓 연결하고 토너먼트 리스트 불러옴
+	this.getTournamentList = async () => {
+		try {
+			await this.connectWebSocket("wait");
+			gameSocket.addEventListener("message", (event) => {
+				const data = JSON.parse(event.data);
+				if (data.game_list) {
+					// 토너먼트 리스트를 받은 경우
+					this.fillTable(data.game_list);
+				} else if (data.message_type === "create") {
+					if (data.result === "success") {
+						// 방 생성 성공 시 처리
+						this.renderWaiting();
+					} else if (data.result === "fail") {
+						// 방 생성 실패 시 처리
+						alert(`${locale.tournament.createFail}`);
+						this.render();
+					}
+				}
+			});
+		} catch (error) {
+			console.error("Error socket connect:", error);
+		}
+	};
 
-    this.fillTable = (gameList) => {
-        const tableBody = this.$element.querySelector("tbody");
-        if (!tableBody) {
-            console.error("Table body element not found.");
-            return;
-        }
+	this.fillTable = (gameList) => {
+		const tableBody = this.$element.querySelector("tbody");
+		if (!tableBody) {
+			console.error("Table body element not found.");
+			return;
+		}
 
-        if (Array.isArray(gameList) && gameList.length > 0) {
-            const tableRows = gameList
-                .map(
-                    (game, index) => `
+		if (Array.isArray(gameList) && gameList.length > 0) {
+			const tableRows = gameList
+				.map(
+					(game, index) => `
 				<tr>
 						<td class="text-center align-middle col-1">${index + 1}</td>
 						<td class="text-center align-middle col-2">
-								<span class="tournamentNameText" style="cursor: pointer; color: #E5CC5E;">${
-                                    game.tournament_name
-                                }</span>
+								<span class="tournamentNameText" style="cursor: pointer; color: #E5CC5E;">${game.tournament_name
+						}</span>
 						</td>
 						<td class="text-center align-middle col-1">${game.wait_num}/4</td>
 				</tr>
 					`
-                )
-                .join("");
+				)
+				.join("");
 
-            tableBody.innerHTML = tableRows;
-            // 토너먼트 리스트 클릭 이벤트 핸들러 등록
-            const tournamentNameTexts = tableBody.querySelectorAll(
-                ".tournamentNameText"
-            );
-            tournamentNameTexts.forEach((text) => {
-                text.addEventListener("click", () => {
-                    // 선택된 토너먼트 이름을 전역 변수 tournamentName에 할당
-                    tournamentName = text.textContent;
-                    // 선택된 토너먼트로 renderWaiting 호출하여 소켓 연결 시도
-                    this.renderWaiting();
-                });
-            });
-        } else {
-            const language = getCurrentLanguage();
-            const locale = locales[language] || locales.en;
-            tableBody.innerHTML = `<tr><td colspan='3' class='text-left'>${locale.tournament.noGame}</td></tr>`;
-        }
-    };
+			tableBody.innerHTML = tableRows;
+			// 토너먼트 리스트 클릭 이벤트 핸들러 등록
+			const tournamentNameTexts = tableBody.querySelectorAll(
+				".tournamentNameText"
+			);
+			tournamentNameTexts.forEach((text) => {
+				text.addEventListener("click", () => {
+					// 선택된 토너먼트 이름을 전역 변수 tournamentName에 할당
+					tournamentName = text.textContent;
+					// 선택된 토너먼트로 renderWaiting 호출하여 소켓 연결 시도
+					this.renderWaiting();
+				});
+			});
+		} else {
+			const language = getCurrentLanguage();
+			const locale = locales[language] || locales.en;
+			tableBody.innerHTML = `<tr><td colspan='3' class='text-left'>${locale.tournament.noGame}</td></tr>`;
+		}
+	};
 
-    // 새로운 토너먼트 생성시 참가 대기소켓 연결
-    this.renderWaiting = async () => {
-        const language = getCurrentLanguage();
-        const locale = locales[language] || locales.en;
-        try {
-            await this.connectWebSocket(tournamentName); // 기존의 메세지 리스너 같이 사라짐
-            gameSocket.addEventListener("message", (event) => {
-                data = JSON.parse(event.data);
-                if (data.message_type == "wait") {
-                    if (nickname == "" || nickname == data.nickname) {
-                        nickname = data.nickname;
-                        playerNum = data.number;
-                        sessionStorage.setItem("nickname", data.nickname);
-                    }
+	// 새로운 토너먼트 생성시 참가 대기소켓 연결
+	this.renderWaiting = async () => {
+		const language = getCurrentLanguage();
+		const locale = locales[language] || locales.en;
+		try {
+			await this.connectWebSocket(tournamentName); // 기존의 메세지 리스너 같이 사라짐
+			gameSocket.addEventListener("message", (event) => {
+				data = JSON.parse(event.data);
+				if (data.message_type == "wait") {
+					if (nickname == "" || nickname == data.nickname) {
+						nickname = data.nickname;
+						playerNum = data.number;
+						sessionStorage.setItem("nickname", data.nickname);
+					}
 
-                    gameSocket.send(event.data);
+					gameSocket.send(event.data);
 
-                    this.$element.innerHTML = `
+					this.$element.innerHTML = `
 					<div class='text-center h1 text-left tp-color-secondary'>${locale.tournament.waiting}</div>
 					<div class='text-center h1 text-left tp-color-secondary'>${data.total} / 4</div>
 					<div class="text-center">
 							<button id="goBackToListBtn" class="btn tp-btn-primary">${locale.tournament.goBack}</button>
 					</div>
 			`;
-                    // "Go back to list" 버튼 클릭 이벤트 핸들러 등록
-                    const goBackToListBtn =
-                        this.$element.querySelector("#goBackToListBtn");
-                    goBackToListBtn.addEventListener(
-                        "click",
-                        this.goBackToList
-                    );
-                } else if (data.message_type == "ready") {
-                    // this.renderPlaying(data);
-                    // round 저장, 1p, 2p 나랑 versus 저장
-                    round = data.round;
-                    if (data["1p"] == nickname) {
-                        playerNum = "player1";
-                        versusNickname = data["2p"];
-                    } else {
-                        playerNum = "player2";
-                        versusNickname = data["1p"];
-                    }
-                    sessionStorage.setItem("playerNum", playerNum);
-                    sessionStorage.setItem("Data", JSON.stringify(data));
-                    sessionStorage.setItem("gameMode", "tournament_game");
-                    sessionStorage.setItem("tournamentName", tournamentName);
-                    sessionStorage.setItem("versusNickname", versusNickname);
-                    let tournamentURL = `${tournamentName}/${round}`;
-                    sessionStorage.setItem("idValue", tournamentURL);
-                    let targetURL = `https://${process.env.BASE_IP}/tournament_game/${tournamentURL}`;
-                    navigate(targetURL);
-                    // 저장된 토너먼트 모드, 토너먼트방이름, 라운드 합쳐서 스토리지에 저장 후 게임 연결
-                }
-            });
-        } catch (error) {
-            console.error("뭔가;; 잘못됨:", error);
-        }
-    };
+					// "Go back to list" 버튼 클릭 이벤트 핸들러 등록
+					const goBackToListBtn =
+						this.$element.querySelector("#goBackToListBtn");
+					goBackToListBtn.addEventListener(
+						"click",
+						this.goBackToList
+					);
+				} else if (data.message_type == "ready") {
+					// this.renderPlaying(data);
+					// round 저장, 1p, 2p 나랑 versus 저장
+					round = data.round;
+					if (data["1p"] == nickname) {
+						playerNum = "player1";
+						versusNickname = data["2p"];
+					} else {
+						playerNum = "player2";
+						versusNickname = data["1p"];
+					}
+					sessionStorage.setItem("playerNum", playerNum);
+					sessionStorage.setItem("Data", JSON.stringify(data));
+					sessionStorage.setItem("gameMode", "tournament_game");
+					sessionStorage.setItem("tournamentName", tournamentName);
+					sessionStorage.setItem("versusNickname", versusNickname);
+					let tournamentURL = `${tournamentName}/${round}`;
+					sessionStorage.setItem("idValue", tournamentURL);
+					let targetURL = `https://${process.env.BASE_IP}/tournament_game/${tournamentURL}`;
+					navigate(targetURL);
+					// 저장된 토너먼트 모드, 토너먼트방이름, 라운드 합쳐서 스토리지에 저장 후 게임 연결
+				}
+			});
+		} catch (error) {
+			console.error("뭔가;; 잘못됨:", error);
+		}
+	};
 
-    this.goBackToList = () => {
-        // 소켓 연결을 끊음
-        if (gameSocket) {
-            gameSocket.close();
-            gameSocket = null;
-        }
-        // 토너먼트 리스트를 다시 요청
-        this.render();
-    };
+	this.goBackToList = () => {
+		// 소켓 연결을 끊음
+		if (gameSocket) {
+			gameSocket.close();
+			gameSocket = null;
+		}
+		// 토너먼트 리스트를 다시 요청
+		this.render();
+	};
 
-    //소켓 연결시 사용
-    this.connectWebSocket = async (url) => {
-        return new Promise((resolve, reject) => {
-            gameSocket = new WebSocket(
-                `wss://${process.env.BASE_IP}/ws/tournament_game/${url}/`
-            );
-            window.addEventListener("popstate", closeSocket);
-            gameSocket.onopen = () => {
-                resolve(gameSocket);
-            };
+	//소켓 연결시 사용
+	this.connectWebSocket = async (url) => {
+		return new Promise((resolve, reject) => {
+			gameSocket = new WebSocket(
+				`wss://${process.env.BASE_IP}/ws/tournament_game/${url}/`
+			);
+			window.addEventListener("popstate", closeSocket);
+			gameSocket.onopen = () => {
+				resolve(gameSocket);
+			};
 
-            gameSocket.onerror = (error) => {
-                reject(error);
-            };
-        });
-    };
+			gameSocket.onerror = (error) => {
+				reject(error);
+			};
+		});
+	};
 
-    this.state = initialState;
-    this.$element = document.createElement("div");
-    this.$element.className = "content";
-    // 기본적인 SPA 페이지 로드동작
-    this.init = () => {
-        let parent = $("#app");
-        const child = $(".content");
-        if (child) {
-            parent.removeChild(child);
-            parent.appendChild(this.$element);
-        }
-        let body = $("body");
-        const canvas = $("canvas");
-        if (canvas) {
-            body.removeChild(canvas);
-        }
-        this.render();
-    };
-    window.addEventListener(
-        "languageChange",
-        function () {
-            this.render();
-        }.bind(this)
-    );
+	this.state = initialState;
+	this.$element = document.createElement("div");
+	this.$element.className = "content";
+	// 기본적인 SPA 페이지 로드동작
+	this.init = () => {
+		let parent = $("#app");
+		const child = $(".content");
+		if (child) {
+			parent.removeChild(child);
+			parent.appendChild(this.$element);
+		}
+		let body = $("body");
+		const canvas = $("canvas");
+		if (canvas) {
+			body.removeChild(canvas);
+		}
+		this.render();
+	};
+	window.addEventListener(
+		"languageChange",
+		function () {
+			this.render();
+		}.bind(this)
+	);
 
-    this.init();
+	this.init();
 }
 
 export default Tournament;
